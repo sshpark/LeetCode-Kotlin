02de0816b09408aa4b5478f2dbef852330b6e16f
sshpark <huangjiamingwl@icloud.com>
2019-10-30 01:02:29 +0800

    add C++ Solution

--- GenerateReadMe.kt
+++ GenerateReadMe.kt
@@ -1,4 +1,5 @@
 import java.io.File
+
 fun main(args: Array<String>) {
     val file = File("./README.md")
     file.writeText("# LeetCode\n")
@@ -22,40 +23,43 @@ fun main(args: Array<String>) {
     val easyFolder = File("./easy").walk()
     easyFolder.maxDepth(1)
             .filter { it.isFile }
-            .filter { it.extension == "kt" }
+            .filter { it.extension == "kt" || it.extension == "cpp"}
             .forEach { f ->
+                val ext = if (f.extension == "kt") "Kotlin" else "C++"
                 val name = f.name
                 val firstDotIndex = name.indexOf('.', 0)
                 val id = name.substring(0, firstDotIndex)
                 val title = name.substring(firstDotIndex+1, name.length-3)
                 val leetcodeLink = "[$title](https://leetcode.com/problems/$title/description/)"
-                readmeContents.add(Pair(id.toInt(), "| $id | $leetcodeLink | [Kotlin]($f) | Easy |\n"))
+                readmeContents.add(Pair(id.toInt(), "| $id | $leetcodeLink | [$ext]($f) | Easy |\n"))
             }
 
     val mediumFolder = File("./medium").walk()
     mediumFolder.maxDepth(1)
             .filter { it.isFile }
-            .filter { it.extension == "kt" }
+            .filter { it.extension == "kt" || it.extension == "cpp"}
             .forEach { f ->
+                val ext = if (f.extension == "kt") "Kotlin" else "C++"
                 val name = f.name
                 val firstDotIndex = name.indexOf('.', 0)
                 val id = name.substring(0, firstDotIndex)
                 val title = name.substring(firstDotIndex+1, name.length-3)
                 val leetcodeLink = "[$title](https://leetcode.com/problems/$title/description/)"
-                readmeContents.add(Pair(id.toInt(), "| $id | $leetcodeLink | [Kotlin]($f) | Medium |\n"))
+                readmeContents.add(Pair(id.toInt(), "| $id | $leetcodeLink | [$ext]($f) | Medium |\n"))
             }
 
     val hardFolder = File("./hard").walk()
     hardFolder.maxDepth(1)
             .filter { it.isFile }
-            .filter { it.extension == "kt" }
+            .filter { it.extension == "kt" || it.extension == "cpp"}
             .forEach { f ->
+                val ext = if (f.extension == "kt") "Kotlin" else "C++"
                 val name = f.name
                 val firstDotIndex = name.indexOf('.', 0)
                 val id = name.substring(0, firstDotIndex)
                 val title = name.substring(firstDotIndex+1, name.length-3)
                 val leetcodeLink = "[$title](https://leetcode.com/problems/$title/description/)"
-                readmeContents.add(Pair(id.toInt(), "| $id | $leetcodeLink | [Kotlin]($f) | Hard |\n"))
+                readmeContents.add(Pair(id.toInt(), "| $id | $leetcodeLink | [$ext]($f) | Hard |\n"))
             }
     readmeContents.sortBy( { it.first } )
     readmeContents.forEach {
