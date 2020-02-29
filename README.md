95ca03c44182f125c84d1678a493f56c6f0946d2
sshpark <huangjiamingwl@icloud.com>
2020-02-29 23:59:08 +0800

    add problem 20

--- README.md
+++ README.md
@@ -19,6 +19,7 @@
 | 17 | [letter-combinations-of-a-phone-number](https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/) | [Kotlin](./medium/17.letter-combinations-of-a-phone-number.kt) | Medium |
 | 18 | [4-sum](https://leetcode.com/problems/4-sum/description/) | [Kotlin](./medium/18.4-sum.kt) | Medium |
 | 19 | [remove-nth-node-from-end-of-list](https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/) | [Kotlin](./medium/19.remove-nth-node-from-end-of-list.kt) | Medium |
+| 20 | [valid-parentheses](https://leetcode.com/problems/valid-parentheses/description/) | [Kotlin](./easy/20.valid-parentheses.kt) | Easy |
 | 22 | [generate-parentheses](https://leetcode.com/problems/generate-parentheses/description/) | [Kotlin](./medium/22.generate-parentheses.kt) | Medium |
 | 24 | [swap-nodes-in-pairs](https://leetcode.com/problems/swap-nodes-in-pairs/description/) | [Kotlin](./medium/24.swap-nodes-in-pairs.kt) | Medium |
 | 26 | [remove-duplicates-from-sorted-array](https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/) | [Kotlin](./easy/26.remove-duplicates-from-sorted-array.kt) | Easy |
---
+++ easy/20.valid-parentheses.kt
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode id=20 lang=kotlin
+ *
+ * [20] Valid Parentheses
+ *
+ * https://leetcode.com/problems/valid-parentheses/description/
+ *
+ * algorithms
+ * Easy (37.80%)
+ * Likes:    4179
+ * Dislikes: 197
+ * Total Accepted:    862.8K
+ * Total Submissions: 2.3M
+ * Testcase Example:  '"()"'
+ *
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and
+ * ']', determine if the input string is valid.
+ * 
+ * An input string is valid if:
+ * 
+ * 
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * 
+ * 
+ * Note that an empty string is also considered valid.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "()"
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "()[]{}"
+ * Output: true
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "(]"
+ * Output: false
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: "([)]"
+ * Output: false
+ * 
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input: "{[]}"
+ * Output: true
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    fun isValid(s: String): Boolean {
+        var stk = Stack<Char>()
+        stk.push('#')
+        for (i in s) {
+            if (i == '(') stk.push(i)
+            if (i == '[') stk.push(i)
+            if (i == '{') stk.push(i)
+            if (i == ')') {
+                val c = stk.pop()
+                if (c != '(') return false
+            }
+            if (i == ']') {
+                val c = stk.pop()
+                if (c != '[') return false
+            }
+            if (i == '}') {
+                val c = stk.pop()
+                if (c != '{') return false
+            }
+        }
+        if (stk.size > 1) return false
+        return true
+    }
+}
+// @lc code=end
+
